742ac0b1884df6d2b6c10cfce4f6bfe6c7f718bd
Khalilox-work <aarabakhalil03@gmail.com>
2025-05-01 14:38:23 +0100

    Database Creation

---
+++ DjangoProject/SAV/Appli_SAV/models.py
@@ -0,0 +1,8 @@
+
+
+# Create your models here.
+from django.db import models
+
+class Example(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
